44c2093cd7b4648742333032ccc40c83c4fafda1
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-11-07 23:34:57 -0500

    Display values of button pressed in display label

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
     
-    }
+        isFinishedTypingNumber = true
+        
+        }
+    
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        // display label to show values of button pressed
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        
     }
 
 }
 
+}
