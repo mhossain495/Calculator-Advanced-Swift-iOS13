99037e574d1c1d8c52aeaef95e6ccbc52843fe50
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-11-12 20:09:30 -0500

    Add functions to clear, convert to %, or invert value

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -27,6 +27,16 @@ class ViewController: UIViewController {
             fatalError("Cannot convert display label text to a Double")
         }
         
+        // Functions to clear, convert to %, or invert value
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                    displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
         
         }
     
